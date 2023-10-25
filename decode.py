776f812fc26237297f5f1d556288eade9b01460e
Daniel Hernandez <dhernandez8@ufl.edu>
2023-10-25 14:20:57 -0400

    fix decode method for values > 8

--- decode.py
+++ decode.py
@@ -2,7 +2,7 @@ def decode(password):
     decoded_password = ''
     for character in password:
         if (int(character) - 3) < 0:
-            decoded_password += str(10 - (int(character) - 3))
+            decoded_password += str(10 + (int(character) - 3))
         else:
             decoded_password += str(int(character) - 3)
     return int(decoded_password)
